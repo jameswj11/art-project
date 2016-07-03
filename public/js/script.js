998afe9c80c4b3f410b9922275d98f375b9a4376
James Johnson <jameswj111@gmail.com>
2016-07-03 19:14:17 -0400

    added saving

--- public/js/script.js
+++ public/js/script.js
@@ -5,6 +5,7 @@ $(function(){
   const $body = $('body')
   const $results = $('.results')
   const $container = $('.container')
+  const $img = $('img')
 
   $('button').click(function(){
     let queryObject = {}
@@ -23,12 +24,26 @@ $(function(){
         data.forEach(function(artwork){
           const $artwork = $('<img>')
             .attr('src', artwork.webImage.url)
+            .val(artwork.objectNumber)
           $results.append($artwork)
         })
       }
     })
-
   })
 
+  $results.on('click', 'img', function(){
+    const favorite = {source: $(this).attr('src')}
+    console.log($(this).val + ' image saved!')
+  //click issue resolved by http://stackoverflow.com/questions/26098866/jquery-img-clickfunction-selector-not-working
+  $.ajax({
+    url: '/save',
+    method: 'GET',
+    data: favorite,
+    success: function(data){
+      console.log('success!')
+      // console.log($(this))
+    }
+  })
 
+  })
 })
